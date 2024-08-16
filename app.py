6bc781e949c0987a98a2389ab7a654601a7b3fae
Johnnathan M Lopes <johnnathan_lopes@hotmail.com>
2024-08-16 13:35:17 -0300

    Atualização

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ student_data = data[(data['IdAluno'] == student_id) & (data['IdTurma'] == class_
 if not student_data.empty:
     st.write(f"Previsão para o IdALuno {student_id}")
     st.write(f"Probabilidade de Abandonar a turma: {student_data['ProbabilidadeDesistente'].values[0]:.2%}")
-    st.write(f"Previsão: {'Abandono' if student_data['ProbabilidadeDesistente'].values[0] >= 0.5 else 'Não Abandono'}")
+    st.write(f"Previsão: {'Abandono' if student_data['ProbabilidadeDesistente'].values[0] >= corte else 'Não Abandono'}")
 
     # Plot the probability
     fig, ax = plt.subplots()
