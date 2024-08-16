8a1cd14b5f4fe588bbbf80355ac1103c9cb8fbab
Johnnathan M Lopes <johnnathan_lopes@hotmail.com>
2024-08-16 11:31:35 -0300

    Atualizando o app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Title of the app
 st.title("Probilidade de um aluno do Passos Mágicos abandonar uma turma")
@@ -15,16 +16,29 @@ def load_data():
 # Load the data
 data = load_data()
 
-# Display the dataframe
-st.subheader("Data")
-st.dataframe(data)
+# # Display the dataframe
+# st.subheader("Data")
+# st.dataframe(data)
+
+# Visualização de disbribuição de alunos nas turmas
+st.subheader("Selecione um corte de Probabilidade para observar os alunos com chance de Abandonar")
+corte = st.slider(label='Probabilidade (%) de Abandonar a turma', min_value=0.0, max_value=100.0)
+data['Classe'] = np.where((data['ProbabilidadeDesistente']*100) >= corte, "Abandono", "Não Abandono")
+df_counts = data['Classe'].value_counts(dropna=False).to_frame().rename(columns={'count':'QtdAlunos'})
+df_perc_counts = data['Classe'].value_counts(dropna=False, normalize=True).to_frame().rename(columns={'proportion':'%QtdAlunos'})
+df_perc_counts['%QtdAlunos'] = round(df_perc_counts['%QtdAlunos'] * 100, 2) 
+st.dataframe(df_counts.join(df_perc_counts))
 
 # Select student ID or name to predict
 st.subheader("Selecione um aluno para ver a sua probabilidade de abandono")
 student_id = st.selectbox("Escolha um IdAluno", data['IdAluno'].unique())
 
+if student_id:
+    st.subheader("Selecione uma turma para o aluno")
+    class_id = st.selectbox("Escolha um IdTurma", data[data['IdAluno'] == student_id]['IdTurma'].unique())
+
 # Display selected student's prediction details
-student_data = data[data['IdAluno'] == student_id]
+student_data = data[(data['IdAluno'] == student_id) & (data['IdTurma'] == class_id)]
 
 if not student_data.empty:
     st.write(f"Previsão para o IdALuno {student_id}")
